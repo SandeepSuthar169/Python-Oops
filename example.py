6effe8c988e941f1fec522c37a7f2154d34b8dba
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-04-26 17:35:27 +0530

    sample codes commit

---
+++ example.py
@@ -0,0 +1,10 @@
+lst = [1, 2, 3]
+str = "I am Data scientist"
+list = 5000000
+
+# print(type(lst))
+# print(type(str))
+# print(type(list))
+
+# lst = lst.index(3)
+# print(lst)
---
+++ oops.py
@@ -0,0 +1,24 @@
+class employee:  # initiale class
+    # dunder method/ constructor 
+    def __init__(self):
+        print("started executing attributes or data")
+        self.id = 123
+        self.salary = 5000000 
+        self.designation = "s_MLops_E"
+        print("attributed  or data have been initiated")
+
+
+    def travel(self, destination):
+        print("This travel method was called manually ")
+        print(f"Employee is now tracelling it {destination}")
+
+
+
+
+sam = employee() #obj / instance of the class
+#print(sam.id, sam.salary)
+#sam.travel('vadodra')     # calling method
+#sam.travel("jalor")
+
+print(type(sam))
+
