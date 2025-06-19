4d8adf9e0580acd5ff8ceedc71d4c901e14fd012
Sandeep Suthar <ss.suthar.004@gmail.com>
2025-06-19 06:28:42 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 lst = [1, 2, 3]
 str = "I am Data scientist"
 list = 5000000
-
-# print(type(lst))
-# print(type(str))
-# print(type(list))
-
-# lst = lst.index(3)
-# print(lst)
+#
+print(type(lst))
+print(type(str))
+print(type(list))
+#
+lst = lst.index(3)
+print(lst)
 
 from oops_proj import Chartbook
 user = Chartbook()
